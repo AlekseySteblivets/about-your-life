34f9d493ce427b316001b6f5da1abf7f3284744a
AlekseySteblivets <alsteua@gmail.com>
2023-03-20 22:32:10 +0200

    fixed nav styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,27 +3,18 @@ import styles from "./Navigation.module.scss";
 export default function Navigation() {
   return (
     <nav className={styles.nav}>
-      <ul className={styles.menu1}>
+      <ul className={styles.menu}>
         <li className={styles.item}>
           <a href="/">home</a>
         </li>
         <li className={styles.item}>
           <a href="/">portpholio</a>
         </li>
-      </ul>
-      <ul className={styles.menu2}>
         <li className={styles.item}>
           <a href="/">price</a>
         </li>
-      </ul>
-      <ul className={styles.menu3}>
         <li className={styles.item}>
           <a href="/">video</a>
-        </li>
-      </ul>
-      <ul className={styles.menu4}>
-        <li className={styles.item}>
-          <a href="/">photobooks</a>
         </li>
         <li className={styles.item}>
           <a href="/">contact</a>
@@ -32,3 +23,32 @@ export default function Navigation() {
     </nav>
   );
 }
+
+// <nav className={styles.nav}>
+//   <ul className={styles.menu1}>
+//     <li className={styles.item}>
+//       <a href="/">home</a>
+//     </li>
+//     <li className={styles.item}>
+//       <a href="/">portpholio</a>
+//     </li>
+//   </ul>
+//   <ul className={styles.menu2}>
+//     <li className={styles.item}>
+//       <a href="/">price</a>
+//     </li>
+//   </ul>
+//   <ul className={styles.menu3}>
+//     <li className={styles.item}>
+//       <a href="/">video</a>
+//     </li>
+//   </ul>
+//   <ul className={styles.menu4}>
+//     <li className={styles.item}>
+//       <a href="/">photobooks</a>
+//     </li>
+//     <li className={styles.item}>
+//       <a href="/">contact</a>
+//     </li>
+//   </ul>
+// </nav>;
